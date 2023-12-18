149772c63de8eb352e9f75152e77e43192eb66a2
Nikhil Sharma <nikhilsharma1552000@gmail.com>
2023-12-18 13:09:58 +0530

    Calculatore App  using HTML and CSS
     Calculator APP

---
+++ calculator.js
@@ -0,0 +1,43 @@
+let currentInput = '';
+
+function appendNumber(number) {
+    currentInput += number;
+    updateDisplay();
+}
+
+function appendOperator(operator) {
+    currentInput += operator;
+    updateDisplay();
+}
+
+function calculate() {
+    try {
+        currentInput = eval(currentInput).toString();
+        updateDisplay();
+    } catch (error) {
+        currentInput = 'Error';
+        updateDisplay();
+    }
+}
+
+function clearDisplay() {
+    currentInput = '';
+    updateDisplay();
+}
+
+function calculatePercentage() {
+    currentInput = (parseFloat(currentInput) / 100).toString();
+    updateDisplay();
+}
+
+function updateDisplay() {
+    document.getElementById('display').value = currentInput;
+}
+// ... (previous JavaScript code)
+
+function deleteLast() {
+    currentInput = currentInput.slice(0, -1);
+    updateDisplay();
+}
+
+
